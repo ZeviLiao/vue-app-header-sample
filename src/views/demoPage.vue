4004acbf905f6810c098c2207ebafc2353efe478
Zevi Liao <jointjm@gmail.com>
2020-01-10 15:23:55 +0800

    update

--- src/views/demoPage.vue
+++ src/views/demoPage.vue
@@ -2,17 +2,45 @@
   <div class="customer">
     <h1>I'm a customer</h1>
     <p>I see {{ totalTvCount }} TVs!</p>
+    <p v-show="happyStaff">The staff seems happy!</p>
+    <p v-show="!totalTvCount">I can't buy any..</p>
+    <button 
+      :disabled="!totalTvCount"
+      @click="buyTv">
+      Buy TV
+    </button>
+    <button 
+      :disabled="totalTvCount < 2"
+      @click="buyTwoTvs">
+      Buy Two TVs
+    </button>
   </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
 export default {
+  name: 'Customer',
   computed: {
-    ...mapState(['totalTvCount'])
+    totalTvCount () {
+      return this.$store.state.totalTvCount
+    },
+    happyStaff () {
+      // Check in the getter if the staff is happy
+      return this.$store.getters.happyStaff
+    }
+  },
+  methods: {
+    buyTv() {
+      // Dispatch the action to buy a TV
+      this.$store.dispatch('removeTv', 1)
+    },
+    buyTwoTvs() {
+      // Dispatch the action to buy two TVs
+      this.$store.dispatch('removeTv', 2)
+    }
   }
-};
+}
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 </style>
