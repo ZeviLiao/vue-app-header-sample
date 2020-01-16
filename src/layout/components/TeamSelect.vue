a0a5066ef328a5ceac6dc79411749f2759be6ddf
Zevi Liao <jointjm@gmail.com>
2020-01-16 10:43:24 +0800

    empty message

--- src/layout/components/TeamSelect.vue
+++ src/layout/components/TeamSelect.vue
@@ -24,9 +24,14 @@
       </el-dropdown-menu>
     </el-dropdown>&nbsp;&nbsp;
     <div style="display:inline-block;line-height:0">
-      <el-popover placement="bottom" title="Title" width="200" trigger="click">
-        <div>
-          <img :src="('/static/images/empty-msg-box.png')" width="78">
+      <el-popover placement="bottom" width="200" trigger="click">
+        <div style="text-align:center">
+          <div style="padding:1em;">
+            <img :src="('/static/images/empty-msg-box.png')" width="78">
+            <p>尚未收到任何訊息</p>
+          </div>
+          <hr>
+          <p>前往訊息夾</p>
         </div>
         <div slot="reference">
           <el-badge is-dot class="item">
